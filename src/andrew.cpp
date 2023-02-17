634d2b5e59ef8b4ba9090661516a6d678fd40e4a
fleran <fleran@cs.wallawalla.edu>
2023-02-17 01:48:19 +0000

    autosave

---
+++ src/andrew.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void loading() {
+  cout << "     _____     " << endl
+       << " ___/__|__|____  " << endl
+       << "{______________}  " << endl
+       << " <>         <>        " << endl;
+    cout << "Your car is ready for pick up" << endl; 
+}
+
+int main() {
+  loading();
+  return 0;
+}
