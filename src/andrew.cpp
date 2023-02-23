cbbc369d5f8637c402711fbcaf8698017bf93d71
fleran <fleran@cs.wallawalla.edu>
2023-02-23 07:25:47 +0000

    --message=beta1

--- src/andrew.cpp
+++ src/andrew.cpp
@@ -2,12 +2,62 @@
 #include <chrono>
 #include <iomanip>
 #include <iostream>
+#include <ostream>
+#include <string>
 #include <thread>
 using namespace std;
 
+void menu() {
+  char chose = '0';
+  char confirm = 'n';
+  string car = " ";
+  cout << "=====Welcome to <playholder>=====" << endl;
+  do {
+    car = " ";
+    chose = '0';
+    cout << "Would you like to " << endl
+         << " 0) Quit" << endl
+         << " 1) Rent a Car" << endl
+         << " 2) Return a Car" << endl
+         << " 3) See reservation schedule" << endl;
+    cin >> chose;
+    switch (chose) {
+    case '1':
+      cout << "please pick from availible cars typing the car's name" << endl;
+      // calls the cars availible function
+      cin >> car;
+      // calls the price function when added
+      cout << "Your axpoximate total is <price of car>" << endl
+           << "please confirm (y/n)" << endl;
+      cin >> confirm;
+      if (confirm == 'n') {
+        break;
+      }
+      loading();
+      // the whole point is to rent a car if that is done then should it exit?
+      exit(0);
+      break;
+    case '2':
+      // calls a fuction to return a car
+      cout << "Thank You :) " << endl
+           << "Your total is $<days_rented> * <price of car>" << endl;
+      break;
+    case '3':
+      cout << "The car avalible are <list cars to rent>" << endl;
+      break;
+    case '0':
+      cout << "Goodbye :)" << endl;
+      exit(0);
+    default:
+      cout << "please enter a valid option" << endl;
+    }
+
+  } while (chose != '0');
+}
+
 void loading() {
   char a = ' ';
-  for (int i = 100, j = 0; i >= 0; i--) {
+  for (int i = 115, j = 0; i >= 15; i--) {
     cout << endl << endl << endl << endl << endl << endl;
     cout << endl << endl << endl << endl << endl << endl;
     cout << setw(i) << "     _____      " << endl
@@ -24,6 +74,6 @@ void loading() {
 
 // test call, can be deleted when implemented to main
 int main() {
-  loading();
+  menu();
   return 0;
 }
