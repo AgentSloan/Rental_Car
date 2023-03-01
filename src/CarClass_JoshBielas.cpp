d67ac44cd670bee87ce5788046cb4e54ef9ae335
grahlu <grahlu@cs.wallawalla.edu>
2023-03-01 05:44:08 +0000

    autosave

--- src/CarClass_JoshBielas.cpp
+++ src/CarClass_JoshBielas.cpp
@@ -6,20 +6,24 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 // Class will be here
-class Car{
-    private:
-    // Private class attributes
-        bool availability;
-        std::string make;
-        std::string model;
-    public:
-    // Public class methods to set and get the private class attributes
-        bool getAvailability(){ return this-> availability; }
-        void setAvailability(bool available){ this-> availability = available; }
-        std::string getMake(){ return this-> make; }
-        void setMake(std::string make){ this-> make = make; }
-        std::string getModel(){ return this-> model; }
-        void setModel(std::string model){ this-> model = model; }
+class Car {
+private:
+  // Private class attributes
+  bool availability;
+  std::string make;
+  std::string model;
+
+public:
+  // Public class methods to set and get the private class attributes
+  bool getAvailability() {
+    return this->availability;
+  } // Needs to be a vector attribute and the vector type must be a bool#############
+  void setAvailability(bool available) { this->availability = available; }
+  std::string getMake() { return this->make; }
+  void setMake(std::string make) { this->make = make; }
+  std::string getModel() { return this->model; }
+  void setModel(std::string model) { this->model = model; }
 };
