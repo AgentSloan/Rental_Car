f0f39aae25e1564b8b2eee7b2cf23e26ec2946c1
krieko <krieko@cs.wallawalla.edu>
2023-03-02 19:23:48 +0000

    autosave

--- src/Rent_and_Return.cpp
+++ src/Rent_and_Return.cpp
@@ -1,13 +1,17 @@
 #include "Functions_library.h"
 #include <iostream>
 #include <vector>
-void Rent(std::string car) {
+#include "Car_Class.h"
+
+
+void Rent(std::string car, std::string day) {
   std::vector<bool>
       tempcar; // We be a list of instance of car class when finish
   std::cout << "FIXME1" << std::endl;
   for (int i = 0; i < 5; i++) {
     tempcar.push_back(true);
   }
+  
 
   for (int i = 0; i < tempcar.size(); i++) { // Proof of idea will need to edit
     if (tempcar[i] == true) {
