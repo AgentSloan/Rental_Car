cede3ebe511097cbbd90a5406c6634c0a30bfe05
krieko <krieko@cs.wallawalla.edu>
2023-03-03 03:51:12 +0000

    autosave

--- src/Rent_and_Return.cpp
+++ src/Rent_and_Return.cpp
@@ -1,57 +1,82 @@
+#include "Car_Class.cpp"
 #include "Functions_library.h"
 #include <iostream>
 #include <vector>
-#include "Car_Class.cpp"
-#include "Car_Class.h"
+using namespace std;
 
-Car car1;
-car1.setmake("POG");
-
-void Rent(std::string car, int day) {
-    day-= 1;
-  
-  
-
-  for (int i = 0; i < tempcar.size(); i++) { // Proof of idea will need to edit
-    if (tempcar[i] == true) {
-      // std::cout << " This is a a car we don't have sorry"<< std::endl;
-      // if car.getAvailability == True{
-      //ar::car.setAvailability =  False
-      std::cout << "a car is Rented to you" << std::endl;
-      //}
+// tempcar will be a list of car instancest to run through
+double Rent(string car, int day) {
+    int daysrented;
+  cout << "FIXME";
+  cout <<  "How many days are your renting your car"<< endl;
+  cin >> daysrented;
+  /*
+  for (int i = 0; i < tempcar.size(); i++) {
+    if (car == tempcar[i].getModel) {
+      if (tempcar[i].getAvailability(day) == true) {
+        for(int j =0; j < daysrented; i++){
+        tempcar[i].setAvailability(day+i, false)
+        }
+        return 10000.00;
+      } else {
+        return -1; // Requested car is not avalable
+      }
     }
-  }
+  } return -2; // Car is not somthing we have or is missed typed*/
+  return 0;
 }
 
-void printAvailability() {                   // to print avaalbal car to rent
-    std::cout << "FIXME2"<< std::endl;
+void printAvailability(int day) { // to print avaalbal car to rent
+  cout << "FIXME2" << std::endl;
+  /*
+for (int i = 0; i < tempcar.size(); i++) {
+  std::cout << "the avaiabliy cars are:" << std::endl;
+  if (tempcar[i].getAvailability(day) == true) {
+    tempcar[i].printdetails;
+  }
+}*/
+}
+
+void return_car() {
+  string car;
+  cout << "FIXME3" << std::endl;
+  cout << "PLease enter the model of your returned car" << std::endl;
+  cin >> car;
     /*
-  for (int i = 0; i < carlist.size(); i++) { // will hopefulyy be code
-    std::cout << "the avaiabliy cars are:" << std::endl;
-    if (carlist.at(i).getAvailability == true) {
-      car::car.printdetails;
+  for (int i = 0; i < tempcar.size(); i++) {
+    if (tempcar[i].getMake == car) {
+      tempcar[i].setAvailability[placeholder] = true ;
+      cout << "Your rented car is returned" << std::endl;
     }
   }*/
 }
-void return_car() {
-    std::string car;
-    std::cout << "FIXME3"<< std::endl;
-    std::cout << "PLease enter the model of your returned car"<< std::endl;
-    std::cin >> car;
-    /*
-    for (int i = 0; i < tempcar.size(); i++) { // Proof of idea will need to edit
-    if (tempcar[i].getmake == car) {
-        car::car.setAvailability =  True
-        std::cout<< "Your rented car is returned"<< std::endl;
-    }
-    }
-    */
-
-}
 int main() {
-  Rent("honda ploiet");
-  printAvailability();
+  cout << Rent("honda ploiet", 1);
+  printAvailability(1);
   return_car();
   std::cout << "Test is done" << std::endl;
   return 0;
+
+  /*Car car1;
+  car1.setMake("Car A");
+  car1.setAvailability(0, true);
+  Car car2;
+  car2.setMake("Car B");
+  car2.setAvailability(0, true);
+  Car car3;
+  car3.setMake("Car C");
+  car3.setAvailability(0, true);
+  Car car4;
+  car4.setMake("Car D");
+  car4.setAvailability(0, true);
+  Car car5;
+  car4.setMake("Car E");
+  car4.setAvailability(0, true);
+  std::vector <Car> tempcar;
+  tempcar.push_back(car1);
+  tempcar.push_back(car2);
+  tempcar.push_back(car3);
+  tempcar.push_back(car4);
+  tempcar.push_back(car5);
+  cout << (tempcar[0].getAvailability(0));*/
 }
