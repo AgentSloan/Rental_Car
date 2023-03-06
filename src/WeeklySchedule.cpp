7ddf647e4478c3fbd8bbb7fe907134a5bc19ce82
grahlu <grahlu@cs.wallawalla.edu>
2023-03-06 01:11:34 +0000

    autosave

--- src/WeeklySchedule.cpp
+++ src/WeeklySchedule.cpp
@@ -1,21 +1,26 @@
+#include "Car_Class.cpp"
 #include "Functions_library.h"
 #include <cstring>
 #include <iostream>
-#include "Car_Class.cpp"
 using namespace std;
-
+// THIS IS FOR THE FINAL CODE <D
+// car1.getAvailability();
+// vector<bool> car2Availability = car2.getAvailability();
+// vector<bool> car3Availability = car3.getAvailability();
+// vector<bool> car4Availability = car4.getAvailability();
+// vector<bool> car5Availability = car5.getAvailability();
 // Create dummy variables
-Car car1;
-Car car2;
-Car car3;
-Car car4;
-Car car5;
+// Car car1;
+// Car car2;
+// Car car3;
+// Car car4;
+// Car car5;
 // Define Availiabilty vecot
-vector<bool> car1Availability = car1.getAvailability();
-vector<bool> car2Availability = car2.getAvailability();
-vector<bool> car3Availability = car3.getAvailability();
-vector<bool> car4Availability = car4.getAvailability();
-vector<bool> car5Availability = car5.getAvailability();
+vector<bool> car1Availability{true, true, false, true, true, true, true};
+vector<bool> car2Availability{true, true, false, false, false, false, true};
+vector<bool> car3Availability{true, false, false, false, false, false, true};
+vector<bool> car4Availability{true, false, false, false, false, false, true};
+vector<bool> car5Availability{true, true, true, true, true, true, false};
 string model1 = "Corvette";
 string model2 = "F-150";
 string model3 = "Jeep";
@@ -46,7 +51,7 @@ void printWeeklySchedule() {
       cout << "#      ";
     }
   }
-  cout << endl << model3 << "         ";
+  cout << endl << model3 << "          ";
   for (int i = 0; i != 7; i++) {
     if (car3Availability.at(i) == false) {
       cout << "x      ";
@@ -54,7 +59,7 @@ void printWeeklySchedule() {
       cout << "#      ";
     }
   }
-  cout << endl << model4 << "   ";
+  cout << endl << model4 << "         ";
   for (int i = 0; i != 7; i++) {
     if (car4Availability.at(i) == false) {
       cout << "x      ";
@@ -62,7 +67,7 @@ void printWeeklySchedule() {
       cout << "#      ";
     }
   }
-  cout << endl << model5 << "          ";
+  cout << endl << model5 << "   ";
   for (int i = 0; i != 7; i++) {
     if (car5Availability.at(i) == false) {
       cout << "x      ";
