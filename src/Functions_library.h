4adc7358e29cb44bf4b578c35c09f5338f7c9a87
krieko <krieko@cs.wallawalla.edu>
2023-03-06 02:01:50 +0000

    autosave

--- src/Functions_library.h
+++ src/Functions_library.h
@@ -40,10 +40,6 @@ string helloWorld();
 void loading();
 void re_loading();
 void pringWeeklySchedule();
-<<<<<<< HEAD
-void menu(vector<Car>);
-=======
 void menu();
 double Rent(string car, int day);
 void return_car();
->>>>>>> 8d49ed36907b099cadd04577eb87d87cabc26620
