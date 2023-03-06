f990e3584b6ee06df65b1205a3b0f7daa5c36083
bieljo <bieljo@cs.wallawalla.edu>
2023-03-06 01:56:37 +0000

    autosave

--- src/Functions_library.h
+++ src/Functions_library.h
@@ -8,6 +8,34 @@
 using namespace std;
 
 
+//Car Class
+class Car {
+private:
+  // Private class attributes
+  std::vector<bool> availability;
+  std::string make;
+  std::string model;
+
+public:
+  // Constructors and public class methods to set and get the private class attributes
+  Car() {}
+  Car(int index, bool available) {
+    availability.resize(availability.size() + 1);
+    this->availability.at(index) = available;
+  }
+  std::vector<bool> getAvailability() { return this->availability; }
+  void setAvailability(int index, bool available) {
+    availability.resize(availability.size() + 1);
+    this->availability.at(index) = available;
+  }
+  bool getAvailability(int index) { return this->availability.at(index); }
+  std::string getMake() { return this->make; }
+  void setMake(std::string make) { this->make = make; }
+  std::string getModel() { return this->model; }
+  void setModel(std::string model) { this->model = model; }
+};
+
+
 string helloWorld();
 void loading();
 void re_loading();
