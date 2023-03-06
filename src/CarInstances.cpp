a3fa6445d53ceae943a86e9cb71e09affdf2b9cc
grahlu <grahlu@cs.wallawalla.edu>
2023-03-06 01:37:04 +0000

    autosave

--- src/CarInstances.cpp
+++ src/CarInstances.cpp
@@ -1,9 +1,9 @@
 #include "Car_Class.h"
-#include <iostream>
 #include "Functions_library.h"
-#include <cstring>
+#include <iostream>
 using namespace std;
 
+// Create Rental car objects
 Car chevyImpala;
 
 Car fordTaurus;
@@ -14,5 +14,33 @@ Car toyotaCarola;
 
 Car toyotaRav4;
 
-
-
+// Set vehicle Characteristics
+int main() {
+  // Modify the Class Attributes
+  chevyImpala.setMake("Chevy");
+  chevyImpala.setModel("Impala");
+  for (int i = 0; i != 7; i++) {
+    chevyImpala.setAvailability(i, true);
+  }
+  fordTaurus.setMake("Chevy");
+  fordTaurus.setModel("Impala");
+  for (int i = 0; i != 7; i++) {
+    fordTaurus.setAvailability(i, true);
+  }
+  volkswagenPassat.setMake("Chevy");
+  volkswagenPassat.setModel("Impala");
+  for (int i = 0; i != 7; i++) {
+    volkswagenPassat.setAvailability(i, true);
+  }
+  toyotaCarola.setMake("Chevy");
+  toyotaCarola.setModel("Impala");
+  for (int i = 0; i != 7; i++) {
+    toyotaCarola.setAvailability(i, true);
+  }
+  toyotaRav4.setMake("Chevy");
+  toyotaRav4.setModel("Impala");
+  for (int i = 0; i != 7; i++) {
+    toyotaRav4.setAvailability(i, true);
+  }
+  return 0;
+}
