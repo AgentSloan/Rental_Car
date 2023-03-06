781d0a180cb16755322cf75cb3320c9875aa9c72
bieljo <bieljo@cs.wallawalla.edu>
2023-03-06 01:08:23 +0000

    autosave

--- src/Car_Class.cpp
+++ src/Car_Class.cpp
@@ -8,22 +8,22 @@
 #include <string>
 #include <vector>
 
-// Class will be here
-class Car {
-private:
-  // Private class attributes
-  std::vector<bool> availability;
-  std::string make;
-  std::string model;
+// // Class will be here
+// class Car {
+// private:
+//   // Private class attributes
+//   std::vector<bool> availability;
+//   std::string make;
+//   std::string model;
 
-public:
-  // Public class methods to set and get the private class attributes
-  std::vector<bool> getAvailability(int index) { return this->availability; }
-  void setAvailability(int index, bool available) {
-    this->availability.at(index) = available;
-  }
-  std::string getMake() { return this->make; }
-  void setMake(std::string make) { this->make = make; }
-  std::string getModel() { return this->model; }
-  void setModel(std::string model) { this->model = model; }
-};
+// public:
+//   // Public class methods to set and get the private class attributes
+//   std::vector<bool> getAvailability(int index) { return this->availability; }
+//   void setAvailability(int index, bool available) {
+//     this->availability.at(index) = available;
+//   }
+//   std::string getMake() { return this->make; }
+//   void setMake(std::string make) { this->make = make; }
+//   std::string getModel() { return this->model; }
+//   void setModel(std::string model) { this->model = model; }
+// };
--- src/Car_Class.h
+++ src/Car_Class.h
@@ -8,26 +8,6 @@
 #include <string>
 #include <vector>
 
-// // Class will be here
-// class Car {
-// private:
-//   // Private class attributes
-//   std::vector <bool> availability;
-//   std::string make;
-//   std::string model;
-
-// public:
-//   // Public class methods to set and get the private class attributes
-//   Car() {}
-//   Car(bool available) { this->availability = available; }
-//   std::vector<bool> getAvailability(int index) { return this->availability;
-//   void setAvailability(bool available) { this->availability = available; }
-//   std::string getMake() { return this->make; }
-//   void setMake(std::string make) { this->make = make; }
-//   std::string getModel() { return this->model; }
-//   void setModel(std::string model) { this->model = model; }
-// };
-
 // Class will be here
 class Car {
 private:
@@ -37,15 +17,18 @@ private:
   std::string model;
 
 public:
-
   Car() {}
-  Car(int index, bool available) { availability.resize(availability.size()+1); this->availability.at(index) = available; }
+  Car(int index, bool available) {
+    availability.resize(availability.size() + 1);
+    this->availability.at(index) = available;
+  }
   // Public class methods to set and get the private class attributes
   std::vector<bool> getAvailability() { return this->availability; }
   void setAvailability(int index, bool available) {
-    availability.resize(availability.size()+1);
+    availability.resize(availability.size() + 1);
     this->availability.at(index) = available;
   }
+  bool getAvailability(int index) { return this->availability.at(index); }
   std::string getMake() { return this->make; }
   void setMake(std::string make) { this->make = make; }
   std::string getModel() { return this->model; }
