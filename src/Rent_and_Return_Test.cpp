1de14f55b157e73f53287411783ea44f7f1bd493
krieko <krieko@cs.wallawalla.edu>
2023-03-07 03:59:44 +0000

    autosave

---
+++ src/Rent_and_Return_Test.cpp
@@ -0,0 +1,53 @@
+#include "Rent_and_Return.h"
+#include "WeeklySchedule.h"
+#include "menu.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+// Need work to over files to run properly
+/*
+// Create Rental car objects
+Car chevyImpala;
+Car fordTaurus;
+Car volkswagenPassat;
+Car toyotaCorolla;
+Car hondaPilot;
+
+// Set vehicle Characteristics
+int main() {
+  string car = "";
+  // Modify the Class Attributes
+  chevyImpala.setMake("Chevy");
+  chevyImpala.setModel("Impala");
+  for (int i = 0; i != 7; i++) {
+    chevyImpala.setAvailability(i, true);
+  }
+  fordTaurus.setMake("Ford");
+  fordTaurus.setModel("Taurus");
+  for (int i = 0; i != 7; i++) {
+    fordTaurus.setAvailability(i, true);
+  }
+  volkswagenPassat.setMake("Volkswagen");
+  volkswagenPassat.setModel("Passat");
+  for (int i = 0; i != 7; i++) {
+    volkswagenPassat.setAvailability(i, true);
+  }
+  toyotaCorolla.setMake("Toyota");
+  toyotaCorolla.setModel("Corolla");
+  for (int i = 0; i != 7; i++) {
+    toyotaCorolla.setAvailability(i, true);
+  }
+  hondaPilot.setMake("Honda");
+  hondaPilot.setModel("Pilot");
+  for (int i = 0; i != 7; i++) {
+    hondaPilot.setAvailability(i, true);
+  }
+  vector<Car> CarInstances{chevyImpala, fordTaurus, volkswagenPassat,
+                           toyotaCorolla, hondaPilot};
+  for (int i = 0; i != 5; i++) {
+    cout << CarInstances[i].getMake();
+  }
+
+  cout << Rent(car, 0, CarInstances);
+  return 0;
+}*/
