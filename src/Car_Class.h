74c6fd44dc70a79929a0b2d7720fdb93c7b2657e
bieljo <bieljo@cs.wallawalla.edu>
2023-03-07 03:31:39 +0000

    autosave

--- src/Car_Class.h
+++ src/Car_Class.h
@@ -13,26 +13,31 @@ private:
   double price;
 
 public:
-  // Constructors and public class methods to set and get the private class attributes
+  // Constructors
   Car() {}
   Car(int index, bool available) {
     availability.resize(availability.size() + 1);
     this->availability.at(index) = available;
   }
+  // Methods to get and set availability
   std::vector<bool> getAvailability() { return this->availability; }
   void setAvailability(int index, bool available) {
     availability.resize(availability.size() + 1);
     this->availability.at(index) = available;
   }
   bool getAvailability(int index) { return this->availability.at(index); }
+  // Methods to get and set make
   std::string getMake() { return this->make; }
   void setMake(std::string make) { this->make = make; }
+  // Methods to get and set model
   std::string getModel() { return this->model; }
   void setModel(std::string model) { this->model = model; }
+  // methods to get and set daysRented
   void setDaysRented(int days){
       this->daysRented = days;
   }
   int getDaysRented(){ return this->daysRented; }
+  // methods to get and set price
   void setPrice(double price){
       this->price = price;
   }
