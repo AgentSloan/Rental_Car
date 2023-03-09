0e65f6086f796d11eafe0c56b6b0e8bd962574dd
krieko <krieko@cs.wallawalla.edu>
2023-03-09 19:17:51 +0000

    autosave

--- src/Rent_and_Return.cpp
+++ src/Rent_and_Return.cpp
@@ -16,7 +16,7 @@ double Rent(string car, int day, vector<Car>& tempcar) {
       if (car == tempcar[i].getModel()) {
         if ((DaysRented + day) >= 0 && (DaysRented + day) < 7) {
           if (tempcar[i].getAvailability(day) == true) {
-            for (int j = 1; j < DaysRented; j++) {
+            for (int j = 1; j <= DaysRented; j++) {
               tempcar[i].setAvailability((day+ j), false);
               tempcar[i].setDaysRented(DaysRented);
               //cout << tempcar[i].getModel() << " " << tempcar[i].getDaysRented()
